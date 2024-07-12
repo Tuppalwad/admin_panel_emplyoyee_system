91a76223f34338e97701327cbd17eb031ee5340d
Tuppalwad <vtuppalwad@gmail.com>
2024-07-12 13:58:04 +0530

    project setup done

---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import DropdownBox from "./DropdownBox";
+import Header from "./Header";
+import InputBox from "./InputBox";
+import Sidebar from "./Sidebar";
+
+export {Button,DropdownBox,Header,InputBox,Sidebar}
