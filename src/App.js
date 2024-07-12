a7500edd084e02422b39b5b75dce2a9af1736a97
Tuppalwad <vtuppalwad@gmail.com>
2024-07-12 15:31:22 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register'
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path='/signup' element={<Register/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
